13dc67753bfdbfc10150792834f9a01072b267c0
RamonLS1509 <ramonlopezsalmeron@gmail.com>
2025-11-10 12:09:13 +0100

    Retoques Finales

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 /**
  * @OA\Tag(
